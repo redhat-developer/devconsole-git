83f76ff3878e206f0c23957b575346bf93e7dcad
Matous Jobanek <mjobanek@redhat.com>
2019-04-04 10:01:31 +0200

    fix: sort languages correctly-by numbers not strings

--- pkg/git/langs.go
+++ pkg/git/langs.go
@@ -1,40 +1,44 @@
 package git
 
 import (
-	"fmt"
 	"sort"
 )
 
 // SortLanguagesWithInts sorts languages from the given map to a slice where the first one is with the highest number
 func SortLanguagesWithInts(langsWithSizes map[string]int) []string {
-	var contentSizes []string
-	reversedMap := map[string]string{}
+	var contentSizes []int
+	reversedMap := make(map[int][]string)
 	for lang, size := range langsWithSizes {
-		key := fmt.Sprintf("%d_%s", size, lang)
-		reversedMap[key] = lang
-		contentSizes = append(contentSizes, key)
+		if _, ok := reversedMap[size]; !ok {
+			contentSizes = append(contentSizes, size)
+		}
+		reversedMap[size] = append(reversedMap[size], lang)
 	}
-	return sortLanguages(contentSizes, reversedMap)
+	sort.Ints(contentSizes)
+
+	var sortedLangs []string
+	for i := len(contentSizes) - 1; i >= 0; i-- {
+		sortedLangs = append(sortedLangs, reversedMap[contentSizes[i]]...)
+	}
+	return sortedLangs
 }
 
 // SortLanguagesWithFloats32 sorts languages from the given map to a slice where the first one is with the highest number
 func SortLanguagesWithFloats32(langsWithSizes map[string]float32) []string {
-	var contentSizes []string
-	reversedMap := map[string]string{}
-	for lang, size := range langsWithSizes {
-		key := fmt.Sprintf("%f_%s", size, lang)
-		reversedMap[key] = lang
-		contentSizes = append(contentSizes, key)
+	var contentSizes []float64
+	reversedMap := make(map[float64][]string)
+	for lang, size32 := range langsWithSizes {
+		size := float64(size32)
+		if _, ok := reversedMap[size]; !ok {
+			contentSizes = append(contentSizes, size)
+		}
+		reversedMap[size] = append(reversedMap[size], lang)
 	}
-	return sortLanguages(contentSizes, reversedMap)
-}
-
-func sortLanguages(contentSizes []string, reversedMap map[string]string) []string {
-	sort.Strings(contentSizes)
+	sort.Float64s(contentSizes)
 
 	var sortedLangs []string
 	for i := len(contentSizes) - 1; i >= 0; i-- {
-		sortedLangs = append(sortedLangs, reversedMap[contentSizes[i]])
+		sortedLangs = append(sortedLangs, reversedMap[contentSizes[i]]...)
 	}
 	return sortedLangs
 }
