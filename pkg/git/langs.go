5eb83c25810e4a913e1279bdd470a37f43eaa941
Matous Jobanek <mjobanek@redhat.com>
2019-04-04 12:42:30 +0200

    use my own sorter

--- pkg/git/langs.go
+++ pkg/git/langs.go
@@ -5,40 +5,42 @@ import (
 )
 
 // SortLanguagesWithInts sorts languages from the given map to a slice where the first one is with the highest number
-func SortLanguagesWithInts(langsWithSizes map[string]int) []string {
-	var contentSizes []int
-	reversedMap := make(map[int][]string)
-	for lang, size := range langsWithSizes {
-		if _, ok := reversedMap[size]; !ok {
-			contentSizes = append(contentSizes, size)
-		}
-		reversedMap[size] = append(reversedMap[size], lang)
+func SortLanguagesWithInts(languages map[string]int) []string {
+	var langsWithRatio []langWithRatio
+	for lang, ratio := range languages {
+		langsWithRatio = append(langsWithRatio, langWithRatio{ratio: float64(ratio), lang: lang})
 	}
-	sort.Ints(contentSizes)
+	return sortLangs(langsWithRatio)
+}
+
+// SortLanguagesWithFloats32 sorts languages from the given map to a slice where the first one is with the highest number
+func SortLanguagesWithFloats32(languages map[string]float32) []string {
+	var langsWithRatio []langWithRatio
+	for lang, ratio := range languages {
+		langsWithRatio = append(langsWithRatio, langWithRatio{ratio: float64(ratio), lang: lang})
+	}
+	return sortLangs(langsWithRatio)
+}
+
+func sortLangs(langsWithRatio []langWithRatio) []string {
+	sort.Sort(byRatio(langsWithRatio))
 
 	var sortedLangs []string
-	for i := len(contentSizes) - 1; i >= 0; i-- {
-		sortedLangs = append(sortedLangs, reversedMap[contentSizes[i]]...)
+	for _, sortedLang := range langsWithRatio {
+		sortedLangs = append(sortedLangs, sortedLang.lang)
 	}
 	return sortedLangs
 }
 
-// SortLanguagesWithFloats32 sorts languages from the given map to a slice where the first one is with the highest number
-func SortLanguagesWithFloats32(langsWithSizes map[string]float32) []string {
-	var contentSizes []float64
-	reversedMap := make(map[float64][]string)
-	for lang, size32 := range langsWithSizes {
-		size := float64(size32)
-		if _, ok := reversedMap[size]; !ok {
-			contentSizes = append(contentSizes, size)
-		}
-		reversedMap[size] = append(reversedMap[size], lang)
-	}
-	sort.Float64s(contentSizes)
+type langWithRatio struct {
+	ratio float64
+	lang  string
+}
 
-	var sortedLangs []string
-	for i := len(contentSizes) - 1; i >= 0; i-- {
-		sortedLangs = append(sortedLangs, reversedMap[contentSizes[i]]...)
-	}
-	return sortedLangs
+type byRatio []langWithRatio
+
+func (a byRatio) Len() int      { return len(a) }
+func (a byRatio) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
+func (a byRatio) Less(i, j int) bool {
+	return a[i].ratio > a[j].ratio
 }
