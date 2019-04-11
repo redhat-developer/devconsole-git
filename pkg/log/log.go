2e10c687328a4d0b82cee1c07e3688454a62d7c1
Matous Jobanek <mjobanek@redhat.com>
2019-04-11 09:18:54 +0200

    check GitSource for not being nil before logging

--- pkg/log/log.go
+++ pkg/log/log.go
@@ -6,13 +6,14 @@ import (
 )
 
 func LogWithGSValues(log logr.Logger, gitSource *v1alpha1.GitSource, additional ...interface{}) logr.Logger {
-	values := []interface{}{
-		"name", gitSource.ObjectMeta.Name,
-		"url", gitSource.Spec.URL,
-		"ref", gitSource.Spec.Ref,
-		"flavor", gitSource.Spec.Flavor,
+	var values []interface{}
+	if gitSource != nil {
+		values = []interface{}{
+			"name", gitSource.ObjectMeta.Name,
+			"url", gitSource.Spec.URL,
+			"ref", gitSource.Spec.Ref,
+			"flavor", gitSource.Spec.Flavor,
+		}
 	}
-	values = append(values, additional...)
-
-	return log.WithValues(values...)
+	return log.WithValues(append(values, additional...)...)
 }
